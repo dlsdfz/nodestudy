f2857e5ad6e991095f735a783dc2f69180dcaadb
dlsdfz <305716873@qq.com>
2018-06-06 11:14:05 +0800

    fisrt commit

---
+++ eg/2018.06.06 gzip/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: lc 
+ * @Date: 2018-06-06 10:11:21 
+ * @Last Modified by: lc
+ * @Last Modified time: 2018-06-06 10:29:33
+ */
+const fs = require('fs');
+const zlib = require('zlib');
+
+const gzip = zlib.createGzip();
