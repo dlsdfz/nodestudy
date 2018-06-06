aad667b4bc78986f5c40f295a5c57918e14ea80c
dlsdfz <305716873@qq.com>
2018-06-06 15:20:08 +0800

    dns

---
+++ eg/2018.06.06 dns/index.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: lc 
+ * @Date: 2018-06-06 14:26:45 
+ * @Last Modified by:   lc 
+ * @Last Modified time: 2018-06-06 14:26:45 
+ */
--- eg/2018.06.06 gzip/index.js
+++ eg/2018.06.06 gzip/index.js
@@ -2,9 +2,12 @@
  * @Author: lc 
  * @Date: 2018-06-06 10:11:21 
  * @Last Modified by: lc
- * @Last Modified time: 2018-06-06 10:29:33
+ * @Last Modified time: 2018-06-06 14:11:15
  */
 const fs = require('fs');
 const zlib = require('zlib');
 
 const gzip = zlib.createGzip();
+const inFile=fs.createReadStream('./file/text.txt');
+const out =fs.createWriteStream('./file/text.txt.gz');
+inFile.pipe(gzip).pipe(out);
